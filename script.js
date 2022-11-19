c965a064a9bc499347f0bdacdeb3f59d43642eb4
Wixom78666 <jswix4@gmail.com>
2022-11-19 17:19:40 -0600

    added variables to js

---
+++ script.js
@@ -0,0 +1,38 @@
+// Variables
+
+var questAns = document.querySelector("#gameBox");
+var startBtn = document.querySelector("#ignition");
+var tik = document.querySelector("#tiktok");
+var wrap = document.querySelector("#perim");
+var points = 0;
+var whichQues = 0;
+
+var preguntas = [
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    }
+];
+
+
